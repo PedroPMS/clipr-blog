2ed59ea7005f9386d2a5457095f9cfe11a971f46
Pedro Paulo Silva <p.paulo0512@gmail.com>
2021-11-29 19:01:27 -0300

    docs: documenting the users controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
 use FOS\RestBundle\View\View;
 
 class UserController extends AbstractFOSRestController
@@ -12,6 +15,21 @@ class UserController extends AbstractFOSRestController
     /**
      * @Rest\Get("/api/users", name="users_list")
      * @Rest\View(serializerGroups={"user"})
+     *
+     * List the rewards of the specified user.
+     *
+     * This call takes into account all confirmed awards, but not pending or refused awards.
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns the rewards of an user",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type= App\Entity\User::class, groups={"user"}))
+     *     )
+     * )
+     * @OA\Tag(name="user")
+     * @Security(name="Bearer")
      */
     public function index(UserRepository $userRepository): View
     {
@@ -25,6 +43,18 @@ class UserController extends AbstractFOSRestController
     /**
      * @Rest\Get("/api/profile", name="user_profile")
      * @Rest\View(serializerGroups={"user"})
+     *
+     * List the rewards of the specified user.
+     *
+     * This call takes into account all confirmed awards, but not pending or refused awards.
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns the rewards of an user",
+     *     @OA\JsonContent(ref=@Model(type= App\Entity\User::class, groups={"user"}))
+     * )
+     * @OA\Tag(name="user")
+     * @Security(name="Bearer")
      */
     public function show(UserRepository $userRepository): View
     {
